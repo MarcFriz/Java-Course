63841c362049464a26795a4dffe9be2f40b90e77
MarcFriz <36757567+MarcFriz@users.noreply.github.com>
2018-07-13 08:34:19 +0200

    Init Setup

---
+++ Battleship/src/Start/Main.java
@@ -0,0 +1,14 @@
+package Start;
+
+
+import Classes.*;
+import Interfaces.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("We start here:");
+
+
+    }
+}
